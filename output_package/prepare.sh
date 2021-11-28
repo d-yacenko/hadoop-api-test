c1fef24c75b8d2434ecae5a6ac93a4b88c887a4e
d-yacenko <d.yacenko@gmail.com>
2021-11-28 16:01:35 +0300

    --

--- output_package/prepare.sh
+++ output_package/prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm pkg.zip 2> /dev/null
 cp ../target/test-0.0.1-SNAPSHOT-jar-with-dependencies.jar ../target/lib/ ../conf/ . -rf
 zip -r pkg.zip lib/ conf/ test-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 rm -rf lib/ conf/ test-0.0.1-SNAPSHOT-jar-with-dependencies.jar
